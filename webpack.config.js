21dec06667808404f45bb8f8ee8e330912dd6d9c
monmartius <monmarius@gmail.com>
2019-03-11 17:26:34 +0700

    + precss

--- webpack.config.js
+++ webpack.config.js
@@ -31,14 +31,36 @@ module.exports = {
             },
 
 
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
+            // {
+            //     test: /\.scss$/,
+            //     use: [
+            //         MiniCssExtractPlugin.loader,
+            //         'css-loader',
+            //         'sass-loader'
+            //     ]
+            // },
+
+
+  {
+    test: /\.(scss)$/,
+    use: [{
+      loader: MiniCssExtractPlugin.loader, // inject CSS to page
+    }, {
+      loader: 'css-loader', // translates CSS into CommonJS modules
+    }, {
+      loader: 'postcss-loader', // Run post css actions
+      options: {
+        plugins: function () { // post css plugins, can be exported to postcss.config.js
+          return [
+            require('precss'),
+            require('autoprefixer')
+          ];
+        }
+      }
+    }, {
+      loader: 'sass-loader' // compiles SASS to CSS
+    }]
+  },
 
 
   // {
